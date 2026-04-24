42a3be8a7d5fb1acc77b3a280af2241ddf114ee3
Juan Pablo Cortez Mosquera <63519906+JuanpaCortez93@users.noreply.github.com>
2026-04-23 23:11:58 -0500

    feat: movieheader added

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,16 +1,30 @@
-import { getMovieByIdAction } from "@/core/actions/movie/get-movie-by-id.action";
+import MovieHeader from "@/presentation/components/movie/MovieHeader";
+import useMovie from "@/presentation/hooks/useMovie";
 import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { ActivityIndicator, ScrollView, Text, View } from "react-native";
 
 const MovieScreen = () => {
   const { id } = useLocalSearchParams();
 
-  getMovieByIdAction(+id);
+  const { movieQuery } = useMovie(+id);
+
+  if (movieQuery.isLoading || !movieQuery.data) {
+    return (
+      <View className="flex flex-1 justify-center items-center">
+        <ActivityIndicator className="mb-4" color="purple" size={30} />
+        <Text>Wait a moment</Text>
+      </View>
+    );
+  }
 
   return (
-    <View>
-      <Text>MovieScreen</Text>
-    </View>
+    <ScrollView>
+      <MovieHeader
+        originalTitle={movieQuery.data.originalTitle}
+        poster={movieQuery.data.poster}
+        title={movieQuery.data.title}
+      />
+    </ScrollView>
   );
 };
 
